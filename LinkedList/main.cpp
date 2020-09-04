4b816497c6f1f3e1a3ff38c7c7611300a642f922
Artur Lozovskyi <lozartval@gmail.com>
2020-09-04 15:06:05 +0300

    Initial Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Artur Lozovskyi on 9/4/20.
+//  Copyright © 2020 Artur Lozovskyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
